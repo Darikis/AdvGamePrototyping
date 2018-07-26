ae35db852eb1b2d8d51643d9e341e4a47c69e2ef
Student <Student@aii.stu>
2018-07-26 18:55:44 -0400

    Wk 3 Start
    Added prototyping and character packages

---
+++ FranticFighting/Assets/Player1Ctrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Ctrl : MonoBehaviour {
+
+    public GameObject Player;
+    public float RunSpeed;
+    public Rigidbody rigi;
+
+	// Use this for initialization
+	void Start () {
+        Player = gameObject;
+        rigi = Player.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+        {
+            rigi.AddForce(Vector3.forward * RunSpeed, ForceMode.Impulse);
+        }
+	}
+}
