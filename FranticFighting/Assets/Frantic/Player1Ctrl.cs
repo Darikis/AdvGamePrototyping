e4cefd80bea264b68bed439f73d86e222a11cefc
Student <Student@aii.stu>
2018-08-02 21:02:05 -0400

    Wk4
    Timer set up

--- FranticFighting/Assets/Frantic/Player1Ctrl.cs
+++ FranticFighting/Assets/Frantic/Player1Ctrl.cs
@@ -6,7 +6,13 @@ public class Player1Ctrl : MonoBehaviour {
 
     public GameObject Player;
     public float RunSpeed;
+    public float MoveVal;
+    public float Zval;
+    public float Xval;
+    public Vector3 Pos;
+    public bool InputReady;
     public Rigidbody rigi;
+    public Timer Timer;
     public Players PlayerCtrl;
     public List<CtrlScheme> ListOfPlayers;
   
@@ -27,11 +33,36 @@ public class Player1Ctrl : MonoBehaviour {
 	void Start () {
         Player = gameObject;
         rigi = Player.GetComponent<Rigidbody>();
+        Pos = Player.transform.position;
+        Zval = Pos.z;
+        Xval = Pos.x;
+        
+        
+       
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        Pos = new Vector3(Xval, 1, Zval);
+
+        if (Timer.timeLeft > 0)
+        {
+            InputReady = true;
+        }
+        else
+        {
+            InputReady = false;
+        }
+        
+        if (InputReady == false)
+        {
+            Timer.timeLeft = 0;
+            Player.transform.position = Pos;
+            StartCoroutine(Pause());
+        }
+        
         foreach (CtrlScheme ctrl in ListOfPlayers)
         {
             if (Input.GetKeyDown(ctrl.B1))
@@ -54,18 +85,34 @@ public class Player1Ctrl : MonoBehaviour {
     }
     void MoveForward()
     {
-        rigi.AddForce(Vector3.forward * RunSpeed, ForceMode.Impulse);
+        Zval = Zval + MoveVal;
+        //rigi.AddForce(Vector3.forward * RunSpeed, ForceMode.Impulse);
+        
     }
     void MoveBackward()
     {
-        rigi.AddForce(Vector3.back * RunSpeed, ForceMode.Impulse);
+        Zval = Zval - MoveVal;
+        //rigi.AddForce(Vector3.back * RunSpeed, ForceMode.Impulse);
+        
     }
     void MoveRight()
     {
-        rigi.AddForce(Vector3.right * RunSpeed, ForceMode.Impulse);
+        Xval = Xval + MoveVal;
+        //rigi.AddForce(Vector3.right * RunSpeed, ForceMode.Impulse);
+        
     }
     void MoveLeft()
     {
-        rigi.AddForce(Vector3.left * RunSpeed, ForceMode.Impulse);
+        Xval = Xval - MoveVal;
+        //rigi.AddForce(Vector3.left * RunSpeed, ForceMode.Impulse);
+        
+    }
+    IEnumerator Pause()
+    {
+        
+        yield return new WaitForSeconds(1f);
+        Timer.timeLeft = 1;
+        Timer.CountDown = true;
+        StopCoroutine(Pause());
     }
 }
---
+++ FranticFighting/Assets/Frantic/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float timeLeft;
+    public Player1Ctrl Player;
+    public Text CountText;
+    public bool CountDown = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        CountText.text = timeLeft.ToString();
+        if (timeLeft > 0 && CountDown == true)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        if (timeLeft <= 0)
+        {
+            
+            Player.InputReady = false;
+            CountDown = false;
+        }
+        
+    }
+    
+}
